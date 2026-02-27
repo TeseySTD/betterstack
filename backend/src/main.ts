9fbe94316ea2b908495cde7d46cc73952064c0d5
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-02-27 23:27:15 +0200

    feat:Add swagger UI

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Betterstack API')
+    .setVersion('1.0')
+    .build();
+
+  app.setGlobalPrefix('api/v1');
+  
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000');
+  console.log('Swagger UI is running on: http://localhost:3000/api/docs');
 }
 bootstrap();
